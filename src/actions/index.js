34399f58ea0f5cf5f2e5bdc9e2252ae574e2cb39
Piyush Khichadiya <kpn9296@gmail.com>
2021-12-03 21:30:10 +0530

    feat: created project with redux, redux-persist and sementic ui

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+export const addTask = (data) => (
+    {type: "ADD", data}
+);
+
+export const updateTask = (data) => (
+    {type: "UPDATE", data}
+);
+
+export const deleteTask = (data) => (
+    {type: "DELETE", data}
+);
